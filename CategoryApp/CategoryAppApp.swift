21c2e7a5ef6971c63bf7d48e2cc3883c3e52cc21
maxmudovilyos75@gmail.com <maxmudovilyos75@gmail.com>
2022-07-25 11:52:47 +0500

    Initial Commit

---
+++ CategoryApp/CategoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryAppApp.swift
+//  CategoryApp
+//
+//  Created by iMtRo on 25/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CategoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
